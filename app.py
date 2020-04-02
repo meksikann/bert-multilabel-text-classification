2a6b6f89d68dd248722dc66e024a77a73033748a
serhii <meksikann@gmail.com>
2020-04-02 16:01:15 +0300

    general sceleton

--- app.py
+++ app.py
@@ -6,38 +6,64 @@ import random
 import numpy as np
 # tf.test.gpu_device_name()
 from tensorflow.python.client import device_lib
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import datetime, os
+
+plt.style.use('classic')
+if not tf.__version__.startswith("2."):
+    raise Exception('Wrong TF version')
 
 # print(device_lib.list_local_devices())
+
 print('BERT with Tensorflow 2.0')
 print(f'Tf version is: {tf.__version__}')
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
+models_folder = os.path.join(current_dir, "models", "multi_cased_L-12_H-768_A-12")
+EPOCHS = 2
+data_dir = 'data'
+max_seq_length = 48
+classes_number = 3
 
 
 def create_tokenizer():
-
-    models_folder = os.path.join(current_dir, "models", "multi_cased_L-12_H-768_A-12")
     vocab_file = os.path.join(models_folder, "vocab.txt")
 
     tokenizer = bert.bert_tokenization.FullTokenizer(vocab_file, do_lower_case=True)
     return tokenizer
 
 
-data_dir = 'data'
-max_seq_length = 48
-classes_number = 3
+def convert_text_into_tokens(tokenizer, sentences):
+    pred_tokens = map(tokenizer.tokenize, sentences)
+    pred_tokens = map(lambda tok: ["[CLS]"] + tok + ["[SEP]"], pred_tokens)
+    pred_token_ids = list(map(tokenizer.convert_tokens_to_ids, pred_tokens))
+
+    pred_token_ids = map(lambda tids: tids + [0] * (max_seq_length - len(tids)), pred_token_ids)
+    pred_token_ids = np.array(list(pred_token_ids))
+
+    return pred_token_ids
+
+
+def generate_samples(data_set, classes_num):
+    x_set = []
+    y_set = []
+
+    for el in data_set:
+        x_set.append(el[2])
+        zeros = [0] * classes_num
+        class_number = el[1]
+        zeros[int(class_number) - 1] = 1  # onehote encode TODO: use existin onhoteencode method
+        y_set.append(zeros)
+
+    return x_set, y_set
 
 
 def preprocess_data(tokenizer):
+    VAL_PERC = 0.8
     filename = os.path.join(current_dir, data_dir, "DEtest.csv")
-    test_filename = os.path.join(current_dir, data_dir, "data_test.csv")
-
     data = []
-    data_test = []
-    train_set = []
-    train_labels = []
-    test_set = []
-    test_labels = []
 
     # TODO: maybe use Pandas?
     with open(filename, encoding='utf-8') as csvFile:
@@ -46,53 +72,46 @@ def preprocess_data(tokenizer):
             data.append(row)
     csvFile.close()
 
-    # TODO: maybe use Pandas?
-    # TODO: make separate validation set
-    # with open(test_filename, encoding='utf-8') as csvFileTest:
-    #     csv_reader_test = csv.reader(csvFileTest, delimiter=";")
-    #     line_count = 0
-    #     for row in csv_reader_test:
-    #         if line_count > 0:
-    #             data_test.append(row)
-    #         line_count +=1
-    # csvFileTest.close()
+    # shuffle
+    data_set = shuffle(data)
 
-    shuffled_set = random.sample(data, len(data))
-    training_set = shuffled_set[0:]
-    shuffled_set_test = random.sample(data_test, len(data_test))
-    testing_set = shuffled_set_test[0:]
+    # split on train test datasets
+    split_v = int(VAL_PERC * len(data_set)) + 1
 
-    for el in training_set:
-        train_set.append(el[2])
-        zeros = [0] * classes_number
-        class_number = el[1]
-        zeros[int(class_number) - 1] = 1  # onehote encode TODO: use existin onhoteencode method
-        train_labels.append(zeros)
+    # split dataset on 80/20
+    training_set, testing_set = data_set[:split_v], data_set[split_v:]
 
-    for el in testing_set:
-        test_set.append(el[1])
-        zeros = [0] * classes_number
-        zeros[int(el[0]) - 1] = 1
-        test_labels.append(zeros)
+    # prepare X and Y for trainset
+    x_train_set, y_train_labels = generate_samples(training_set, classes_number)
+
+    # prepare X and Y for test
+    x_test_set, y_test_labels = generate_samples(testing_set, classes_number)
+
+    print('------------------------------Train set--------------------------------')
+    print(x_train_set)
+    print(y_train_labels)
+    print('---------------------------------Test set ------------------------------')
+    print(x_train_set)
+    print(y_train_labels)
 
     # defineTokenizerConfig(train_set)
 
-    train_tokens = map(tokenizer.tokenize, train_set)
-    train_tokens = map(lambda tok: ["[CLS]"] + tok + ["[SEP]"], train_tokens)
-    train_token_ids = list(map(tokenizer.convert_tokens_to_ids, train_tokens))
+    # train_tokens = map(tokenizer.tokenize, train_set)
+    # train_tokens = map(lambda tok: ["[CLS]"] + tok + ["[SEP]"], train_tokens)
+    # train_token_ids = list(map(tokenizer.convert_tokens_to_ids, train_tokens))
+    #
+    # train_token_ids = map(lambda tids: tids + [0] * (max_seq_length - len(tids)), train_token_ids)
+    train_token_ids = convert_text_into_tokens(tokenizer, x_train_set)
 
-    train_token_ids = map(lambda tids: tids + [0] * (max_seq_length - len(tids)), train_token_ids)
-    train_token_ids = np.array(list(train_token_ids))
+    # test_tokens = map(tokenizer.tokenize, test_set)
+    # test_tokens = map(lambda tok: ["[CLS]"] + tok + ["[SEP]"], test_tokens)
+    # test_token_ids = list(map(tokenizer.convert_tokens_to_ids, test_tokens))
+    #
+    # test_token_ids = map(lambda tids: tids + [0] * (max_seq_length - len(tids)), test_token_ids)
+    test_token_ids = convert_text_into_tokens(tokenizer, x_test_set)
 
-    test_tokens = map(tokenizer.tokenize, test_set)
-    test_tokens = map(lambda tok: ["[CLS]"] + tok + ["[SEP]"], test_tokens)
-    test_token_ids = list(map(tokenizer.convert_tokens_to_ids, test_tokens))
-
-    test_token_ids = map(lambda tids: tids + [0] * (max_seq_length - len(tids)), test_token_ids)
-    test_token_ids = np.array(list(test_token_ids))
-
-    train_labels_final = np.array(train_labels)
-    test_labels_final = np.array(test_labels)
+    train_labels_final = np.array(y_train_labels)
+    test_labels_final = np.array(y_test_labels)
 
     return train_token_ids, train_labels_final, test_token_ids, test_labels_final
 
@@ -100,9 +119,7 @@ def preprocess_data(tokenizer):
 def create_bert_layer():
     global bert_layer
 
-    bert_dir = os.path.join(modelBertDir, "multi_cased_L-12_H-768_A-12")
-
-    bert_params = bert.params_from_pretrained_ckpt(bert_dir)
+    bert_params = bert.params_from_pretrained_ckpt(models_folder)
 
     bert_layer = bert.BertModelLayer.from_params(bert_params, name="bert")
 
@@ -111,15 +128,12 @@ def create_bert_layer():
 
 
 def load_bert_checkpoint():
-    models_folder = os.path.join(modelBertDir, "multi_cased_L-12_H-768_A-12")
     checkpoint_name = os.path.join(models_folder, "bert_model.ckpt")
 
     bert.load_stock_weights(bert_layer, checkpoint_name)
 
 
 #  Add dense layers after my BERT embedded layer with 256 neurons each.
-
-
 def createModel():
     global model
 
@@ -142,28 +156,73 @@ def createModel():
 
 
 def fitModel(training_set, training_label, testing_set, testing_label):
-    checkpoint_name = os.path.join(modelDir, "bert_faq.ckpt")
+    checkpoint_name = os.path.join(models_folder, "bert_faq.ckpt")
 
     # Create a callback that saves the model's weights
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_name,
                                                      save_weights_only=True,
                                                      verbose=1)
 
-    # callback = StopTrainingClassComplete()
+    logdir = os.path.join("logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
 
     history = model.fit(
         training_set,
         training_label,
-        epochs=300,
+        epochs=EPOCHS,
         validation_data=(testing_set, testing_label),
         verbose=1,
-        callbacks=[cp_callback]
+        callbacks=[tensorboard_callback]
     )
+
+    return history
 
 
 # create tokenizer using BERT mode vocab test
 tokenizer = create_tokenizer()
 # preprocess data (split/shuffle etc)
 train_set, train_labels, test_set, test_labels = preprocess_data(tokenizer)
+print('------------------------------Tokenized Train set--------------------------------')
+
 print('X:', train_set)
 print('Y:', train_labels)
+
+print('Create model')
+create_bert_layer()
+# load_bert_checkpoint()
+createModel()
+
+print('Start model fit ....')
+
+fit_history = fitModel(train_set, train_labels, test_set, test_labels)
+
+plt.title('Accuracy')
+plt.plot(fit_history.history['accuracy'], color='blue', label='Train Acc')
+plt.plot(fit_history.history['val_accuracy'], color='red', label='Validation Acc')
+plt.legend()
+
+_ = plt.figure()
+
+plt.plot(fit_history.history['loss'], color='blue', label='Train Loss')
+plt.plot(fit_history.history['val_loss'], color='orange', label='Validation Loss')
+plt.title('Loss')
+plt.legend()
+
+plt.show()
+
+# Todo: save the model weights
+
+# model predict
+
+pred_sentences = [
+    "Beste Arbeitsumgebung",
+    "hoffe, das Management wird das Anreizsystem nicht weiter ändern",
+    "netter Chef"
+]
+
+pred_token_ids = convert_text_into_tokens(tokenizer, pred_sentences)
+res = model.predict(pred_token_ids)
+
+for text, pred in zip(pred_sentences, res):
+    print(" text:", text)
+    print("  res:", pred)
